33cac4b511900e92c3f4eee0e9b517059ca8f019
chradajan <chradajan@gmail.com>
2024-03-18 19:02:46 -0700

    Compile GBA to a library

--- GBA/src/main.cpp
+++ GBA/src/main.cpp
@@ -1,24 +1,15 @@
 #include <GameBoyAdvance.hpp>
-#include <GUI/MainWindow.hpp>
-#include <SDL.h>
-#include <QtWidgets/QApplication>
 
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
+    GameBoyAdvance gba("");
 
-    GUI::MainWindow mainWindow;
-    mainWindow.show();
-    app.exec();
+    if (argc > 1)
+    {
+        gba.LoadGamePak(argv[1]);
+    }
 
-    // GameBoyAdvance gba("");
-
-    // if (argc > 1)
-    // {
-    //     gba.LoadGamePak(argv[1]);
-    // }
-
-    // gba.Run();
+    gba.Run();
 
     return 0;
 }
